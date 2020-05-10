3fbf53de3abd2e1d4b154eedbba8081a65ee0858
ScriptedThunder <35817761+ScriptedThunder@users.noreply.github.com>
2020-05-09 23:01:58 -0400

    a
    a

--- t.sh
+++ t.sh
@@ -1,64 +1,64 @@
-#! / bin / bash
-ART_KB = "https://help.wnpower.com/hc/es/articles/360018540771-Como-instalar-Node-js"
+#!/bin/bash
+ART_KB="https://help.wnpower.com/hc/es/articles/360018540771-Como-instalar-Node-js"
 
-NODE_VERSIONS = $ (wget -O - https://nodejs.org/dist/ 2> / dev / null | sed 's / <[^>] \ +> // g' | grep "latest. * /" | awk '{print $ 1}' | sed 's / latest - // g' | sed 's / latest // g' | sed 's / \ /// g' | sed ': a; N; $! ba ; s / \ n / / g ')
+NODE_VERSIONS=$(wget -O - https://nodejs.org/dist/ 2>/dev/null | sed 's/<[^>]\+>//g' | grep "latest.*/" | awk '{print $1}' | sed 's/latest-//g' | sed 's/latest//g' | sed 's/\///g' | sed ':a;N;$!ba;s/\n/ /g')
 
-if (echo "$ NODE_VERSIONS" | grep "v. * /"> / dev / null); then
-	echo "Unable to connect to https://nodejs.org/dist/, try manual installation: $ ART_KB"
-the end
+if (echo "$NODE_VERSIONS" | grep "v.*/" > /dev/null); then
+	echo "No se puede conectar a https://nodejs.org/dist/, probá la instalación manual: $ART_KB"
+fi
 
-select VERSION in $ NODE_VERSIONS
+select VERSION in $NODE_VERSIONS
 do
-	FILE = $ (wget -O - https://nodejs.org/dist/latest-$VERSION/ 2> / dev / null | sed 's / <[^>] \ +> // g' | grep "linux -arm64.tar.gz "| awk '{print $ 1}')
-	BIN = $ (echo $ FILE | sed 's / .tar.gz $ //')
-	echo "$ FILE will be downloaded ..."
+	FILE=$(wget -O - https://nodejs.org/dist/latest-$VERSION/ 2>/dev/null | sed 's/<[^>]\+>//g' | grep "linux-arm64.tar.gz" | awk '{print $1}')
+	BIN=$(echo $FILE | sed 's/.tar.gz$//')
+	echo "Se bajará $FILE..."
 
 	cd ~
 	
-	echo "Downloading https: //nodejs.org/dist/latest-$VERSION/$FILE ..."
+	echo "Descargando https://nodejs.org/dist/latest-$VERSION/$FILE..."
 	wget https://nodejs.org/dist/latest-$VERSION/$FILE -O ./$FILE
 
-	echo "Unzipping $ FILE ..."
-	tar xvfz $ FILE
+	echo "Descomprimiendo $FILE..."
+	tar xvfz $FILE
 
-	if [-d "./nodes"]; then
-		echo "Node.JS installation already detected, replace? [s / n]"
+	if [ -d "./nodejs" ]; then
+		echo "Ya se detectó una instalación de Node.JS, reemplazar? [s/n]"
 		read PROMPT
-		if echo "$ PROMPT" | grep -iq "^ s"; then
-			rm -rf "./nodes"
+		if echo "$PROMPT" | grep -iq "^s"; then
+			rm -rf "./nodejs"
 		else
-			echo "Aborting."
+			echo "Abortando."
 			exit 1
-		the end
-	the end
+		fi
+	fi
 
-	mv $ BIN nodes
-	rm -f $ FILE
+	mv $BIN nodejs
+	rm -f $FILE
 
-	mkdir ~ / bin
-	cp nodes / bin / node ~ / bin
-	cd ~ / bin
+	mkdir ~/bin
+	cp nodejs/bin/node ~/bin
+	cd ~/bin
 	ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
 
-	echo "Adding PATH ..."
+	echo "Agregando PATH..."
 
-	sed -i '/ PATH = \ $ PATH: \ $ HOME \ / bin / d' ~ / .bashrc
-	sed -i '/ export PATH / d' ~ / .bashrc
+	sed -i '/PATH=\$PATH:\$HOME\/bin/d' ~/.bashrc
+	sed -i '/export PATH/d' ~/.bashrc
  
-	echo 'PATH = $ PATH: $ HOME / bin' >> ~ / .bashrc
-	echo 'export PATH' >> ~ / .bashrc
+	echo 'PATH=$PATH:$HOME/bin' >> ~/.bashrc
+	echo 'export PATH' >> ~/.bashrc
  
-	PATH = $ PATH: $ HOME / bin
+	PATH=$PATH:$HOME/bin
 	export PATH
 
-	echo "Checking installation ..."
+	echo "Comprobando instalación..."
 
-	echo -n "node --version:"
+	echo -n "node --version: "
 	node --version
-	echo -n "npm --version:"
+	echo -n "npm --version: "
 	npm --version
 
-	echo "Ready !. For more info and sample code enter $ ART_KB"
+	echo "¡Listo!. Para más info y código de ejemplo ingresá a $ART_KB"
 
 	exit 1
 done
