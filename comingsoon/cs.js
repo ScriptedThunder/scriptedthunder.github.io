184683366a1048c7363a96f0f75c60c1a1f1c842
ScriptedThunder <jordanwatts1010@gmail.com>
2025-11-12 16:11:47 -0500

    V0.1
    Removed Useless Files
    Updated Favicon
    Removed Useless CSS
    and ETC

--- comingsoon/cs.js
+++ comingsoon/cs.js
@@ -1,6 +1,6 @@
 // Countdown
 // Set the date we're counting down to
-var countDownDate = new Date("March 31, 2019 00:00:00").getTime();
+var countDownDate = new Date("January 1, 2026 00:00:00").getTime();
     
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
